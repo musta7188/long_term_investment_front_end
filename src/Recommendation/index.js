c28995c2447c1042c65900e975b276ffe6184de3
musta7188 <musta7188@hotmail.it>
2020-05-12 20:03:03 +0100

    selected chart component

--- src/Recommendation/index.js
+++ src/Recommendation/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import StockPage from './StocksPage'
-
+import SelectedStock from './SelectedStock'
 export default function index() {
   return (
     <div>
       <StockPage/>
+      <SelectedStock/>
     </div>
   )
 }
