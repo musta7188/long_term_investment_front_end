ce8a3037eb41d6c5d9d37dc1e3c039ac880c3a4f
musta7188 <musta7188@hotmail.it>
2020-05-28 20:17:22 +0100

    route adjistment

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ import path from 'path'
 import express from 'express'
 import serveStatic from 'serve-static'
 
-// const app = express()
-// const PORT = process.env.PORT || 8080
-
-
-// // app.get('*', (req, res) =>{ res.sendFile(path.join(__dirname, '..build/index.html')))
-
-// app.listen(PORT,() => console.log(`listening on port ${PORT}`))
-
 
 var express = require('express');
 var app = express();
@@ -18,10 +10,9 @@ app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(serveStatic('build/'))
-  app.get('*', (req, res) =>{ res.sendFile(path.join(__dirname, '..build/index.html'))
-  
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../Portfolio', 'index.html'));
   });
 }
-
-module.exports = app
