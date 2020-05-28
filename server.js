b05ed4612f4351b8033a8d5f80d9d76b764fbecf
musta7188 <musta7188@hotmail.it>
2020-05-28 18:49:38 +0100

    route adjistment

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('*', (req, res) => {
     res.sendFile(path.join('build', 'index.html'));
-    res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
+    res.sendFile(path.resolve(__dirname, '../Portfolio', 'index.html'));
   });
 }
