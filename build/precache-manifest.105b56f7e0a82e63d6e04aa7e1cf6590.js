e778e185537972f858d721dd442ddd24c800c34b
musta7188 <musta7188@hotmail.it>
2020-08-10 16:24:53 +0100

    change stage

---
+++ build/precache-manifest.105b56f7e0a82e63d6e04aa7e1cf6590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c50ba2b65ab313d12515d85b29adb25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c9ac9875ebaa438d637",
+    "url": "/static/css/main.f5137995.chunk.css"
+  },
+  {
+    "revision": "088e28e07476e621e051",
+    "url": "/static/js/2.debc5d8d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.debc5d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9ac9875ebaa438d637",
+    "url": "/static/js/main.653e6b86.chunk.js"
+  },
+  {
+    "revision": "49e77e4938b73b138d0a",
+    "url": "/static/js/runtime-main.de8a56dc.js"
+  }
+]);
